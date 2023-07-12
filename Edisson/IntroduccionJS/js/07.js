2057ea56f5829f0fff016814ab34608b0dd8cbf5
Edisson Payes <edissonpayes75@gmail.com>
2023-07-11 19:51:09 -0600

    Clase 110: Controlar el orden de las Operaciones

---
+++ Edisson/IntroduccionJS/js/07.js
@@ -0,0 +1,26 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2;
+resultado = (20 + 30) * 2;
+resultado = (100+200+300) * .2; // Es el 20% de descuento del carrito de compras en el parentesis
+resultado = (100+200+300) * 1.16; // El resultado de la suma normal mas el 16% de impuestos
+resultado = (600 + 600) * 1.05; // Se suma primero el resultado de parentesis, luego se le suma el 5% de comision o impuestos tambien.
+
+
+// console.log(resultado); 
+// Incrementos
+
+let puntaje = 10;
+let puntaje1 = 15;
+
+// Incremento en 1 ++
+
+puntaje +=10; // Para sumarle a puntaje 10 otros 10
+
+console.log(puntaje); 
+
+--puntaje1; // Le quita uno a 15
+
+console.log(puntaje1); 
