fa27d9f569e31891b80a1105fce6ca8d0feeeb80
Edisson Payes <edissonpayes75@gmail.com>
2023-07-11 18:05:55 -0600

    Clase 106: String o Cadenas de texto

---
+++ Edisson/IntroduccionJS/js/04.js
@@ -0,0 +1,12 @@
+// String o Cadenas de Texto
+// Pueden ser comillas dobles o snecillas
+// Forma 1
+const producto = "Monitor de 20\"";
+// Forma 2
+const producto2 = String('Monitor 30 pulgadas'); // Usando un constructor
+// Forma 3
+const producto3 = new String('Monitor 50 pulgadas');
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
