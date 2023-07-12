e0ce69fb36bd4d4c18688b5217c9791d2a0da51c
Edisson Payes <edissonpayes75@gmail.com>
2023-07-11 18:38:27 -0600

    Clase 107: Metodos para los String

--- Edisson/IntroduccionJS/js/04.js
+++ Edisson/IntroduccionJS/js/04.js
@@ -1,12 +1,18 @@
 // String o Cadenas de Texto
-// Pueden ser comillas dobles o snecillas
-// Forma 1
-const producto = "Monitor de 20\"";
-// Forma 2
-const producto2 = String('Monitor 30 pulgadas'); // Usando un constructor
-// Forma 3
-const producto3 = new String('Monitor 50 pulgadas');
 
-console.log(producto)
-console.log(producto2)
-console.log(producto3)
+const tweet = 'Aprendiendo JavaScript con el curso de Desarrollo Web Completo';
+const producto2 = 'Monitor 20"';
+const email = 'correo@correo.com'
+
+// length es para la extension
+console.log(tweet.length); // Devuelve cuantas palabras hay en la cadena de texto
+console.log(producto2);
+
+// IndexOf
+console.log(tweet.indexOf('JavaScript')); // En la posicion numero 12 se encuentra JS, empieza desde 0
+console.log(producto2.indexOf('Tablet')); // Cuando el resultado es -1, no lo encontro en la cadena de texto
+console.log(email.indexOf('@'));
+
+//Includes (retorna true o false)
+console.log(tweet.includes('JavaScript')); // usando el include retorna si lo encontro con un true
+console.log(producto2.includes('Tablet')); // retorna si no lo encontro con un false
