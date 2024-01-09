1777c7e7cc8768b13d3f086dec0b571887d89e6e
Edisson Payes <edissonpayes75@gmail.com>
2024-01-08 22:16:03 -0600

    Clase 118: Arrays

--- Edisson/IntroduccionJS/js/13.js
+++ Edisson/IntroduccionJS/js/13.js
@@ -1,5 +1,5 @@
 // Clase para unir objetos
-// Objeto
+// Objetos
 
 const producto = {
     // Propiedad o llave del objeto, si cambia el signo de = por :
@@ -13,7 +13,12 @@ const medidas = {
     medida: '1m'
 }
 
-const nuevoProducto = { ...producto, ...medidas}; // Con los tres puntos se unen los dos objetos
+const color = {
+    amarillo: '12',
+    verde: '11'
+}
+
+const nuevoProducto = { ...producto, ...medidas, ...color}; // Con los tres puntos se unen los dos objetos
 
 // Mostrar mensaje en pantalla
 console.log(producto);
