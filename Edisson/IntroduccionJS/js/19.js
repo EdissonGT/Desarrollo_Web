2cc5b1ff57b8c8ae9705eeb258d7649f847babc2
Edisson Payes <edissonpayes75@gmail.com>
2024-01-11 21:10:23 -0600

    Clase 123: Diferencia de un Método y una Función

---
+++ Edisson/IntroduccionJS/js/19.js
@@ -0,0 +1,6 @@
+const numero1 = 20;
+const numero2 = "20";
+
+console.log( parseInt(numero2)); // parseInt() es una FUNCION que nos ayuda a convertir de String a numero, si es un tipo de INT valido
+console.log( numero1.toString() ); // .toString es un METODO, porque va antes la variable, luego el punto.
+
