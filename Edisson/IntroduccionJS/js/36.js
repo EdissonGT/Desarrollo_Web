1ef51d165bae26f05363ab5f7dcf8bf6441bbb4f
Edisson Payes <edissonpayes75@gmail.com>
2024-01-24 22:03:49 -0600

    Clase 142: Como trabajar con dos consultas async await

--- Edisson/IntroduccionJS/js/36.js
+++ Edisson/IntroduccionJS/js/36.js
@@ -11,11 +11,30 @@ function descargarNuevosClientes() {
     });
 };
 
+function descargarUltimosPedidos() {
+    return new Promise ( resolve => {
+        console.log('Descargando Pedidos Espere...');
+
+        setTimeout ( () => {
+            resolve('Los Pedidos fueron descargados')
+        }, 3000); // Tiene que tomar 5 segundos
+
+    });
+};
+
 async function app() {
     try {
-        const resultado = await descargarNuevosClientes();
-        // Este codigo de abajo se completa hasta que el de arriba haya sido completado
-        console.log(resultado);
+        // const Clientes = await descargarNuevosClientes(); // Estamos llamando a las funciones de arriba
+        // const Pedidos = await descargarUltimosPedidos();
+        // // Este codigo de abajo se completa hasta que el de arriba haya sido completado
+        // console.log(Clientes);
+        // console.log(Pedidos);
+
+        // Se crea un arreglo para poder llamar a las dos funciones, con el Promise.all
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()])
+        // Se muestra el resultado en pantalla y se accede a la posicion del arreglo
+        console.log(resultado[0]);
+        console.log(resultado[1]);
     } catch (error) {
         console.log(error);
     }
@@ -23,4 +42,4 @@ async function app() {
 
 app();
 
-console.log('Hola');
+// console.log('Hola');
