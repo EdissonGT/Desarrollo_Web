5d55e93d2dfedcc0b44b6716fd7c54caa16e3e72
Edisson Payes <edissonpayes75@gmail.com>
2024-01-27 11:24:11 -0600

    Clase 145: Seleccionar Elementos con querySelectorAll y querySeletor

--- Edisson/JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ Edisson/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -1,3 +1,5 @@
+// Todo los selectores empiezon con document.
+
 // querySelector
 
 const heading = document.querySelector('.header__texto h2'); //Retorna 0 o 1 Elementos
@@ -7,12 +9,27 @@ heading.classList.add('nueva__clase'); // Nos permite agregar una nueva clase a 
 
 console.log(heading);
 
-const enlace = document.querySelector('a');
+// const enlace = document.querySelector('a');
 
-console.log(enlace);
+// console.log(enlace);
 
 // querySelectorAll
 
+const enlaces = document.querySelectorAll('.navegacion a'); // Seleccionamos la clase padre y luego la etiqueta de enlace a
+console.log(enlaces[0]);
 
+enlaces[0].textContent = 'Nuevo Texto para Enlace'; // Cambiamos el texto del valor del enlace en la posicion 0 con el arreglo
+
+enlaces[0].href = 'http://google.com'; // con la propiedad .href redireccionamos el nuevo enlace.
+
+enlaces[0].classList.add('nueva__class'); // Con la propiedad classList.add agregamos nueva clase la etiqueta.
+
+enlaces[0].classList.remove('navegacion__enlace'); // Con la propiedad classList.remove eliminamos una clase existente.
+
+console.log(enlaces[0]);
 
 // getElementById
+
+const heading2 = document.getElementById('heading'); // Podemos seleccionar el codigo html con el ID de la etiqueta creada.
+
+console.log(heading2);
