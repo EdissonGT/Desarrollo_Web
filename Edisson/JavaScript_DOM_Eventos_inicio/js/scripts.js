967c175d0a8be9264e349612ae5d775f3d772aa3
Edisson Payes <edissonpayes75@gmail.com>
2024-01-27 11:09:24 -0600

    Clase 144: Seleccionar Elementos con quesySelector

---
+++ Edisson/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,18 @@
+// querySelector
+
+const heading = document.querySelector('.header__texto h2'); //Retorna 0 o 1 Elementos
+
+heading.textContent = 'Nuevo Heading'; // Nos permite cambiar el texto del contenido.
+heading.classList.add('nueva__clase'); // Nos permite agregar una nueva clase a la etiqueta
+
+console.log(heading);
+
+const enlace = document.querySelector('a');
+
+console.log(enlace);
+
+// querySelectorAll
+
+
+
+// getElementById
